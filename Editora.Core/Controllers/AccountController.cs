31f07b2367eef9dd54fcb223533096e993431e5e
Allan Egidio Costa <allan.egidio@outlook.com>
2018-10-30 19:39:04 -0300

    Separando as pastas

--- Editora.Core/Controllers/AccountController.cs
+++ Editora.Core/Controllers/AccountController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using Editora.Core.Models;
+using Editora.Core.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Editora.Core.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Editora.Core.Controllers
 {
--- Editora.Core/ViewModels/AccountViewModels.cs
+++ Editora.Core/ViewModels/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Editora.Core.Models
+namespace Editora.Core.ViewModels
 {
     public class ExternalLoginConfirmationViewModel
     {
